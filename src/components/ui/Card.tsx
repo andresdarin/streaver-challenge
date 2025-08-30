71f14dbe37491bbf2086b878787f6007a58acf8f
Andrés Darín <andres.darin05@gmail.com>
2025-08-30 19:26:54 -0300

    feat: add lucide-react (icons) | Enhance Interface Posts

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react";
 import { Post } from "../../types/post";
 
 interface PostCardProps {
@@ -7,10 +8,13 @@ interface PostCardProps {
 export default function Card({ post }: PostCardProps) {
     return (
         <div className="border rounded p-4 shadow hover:shadow-lg transition">
-            <p className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 mb-2">User ID: {post.userId}</p>
-            <h2 className="font-semibold tracking-tight text-lg leading-snug group-hover:text-primary transition-colors">{post.title}</h2>
+            <p className="inline-flex items-center gap-1 bg-gray-200 rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-gray-300 mb-2">
+                <User className="w-3 h-3" /> User {post.userId}
+            </p>
+            <h2 className="font-semibold tracking-tight text-lg leading-snug group-hover:text-primary transition-colors">
+                {post.title}
+            </h2>
             <p className="text-muted-foreground text-sm leading-relaxed">{post.body}</p>
         </div>
-
     );
 }
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -22,34 +22,67 @@ export default function Posts() {
 
     return (
         <main className="max-w-5xl mx-auto p-4">
-            <h1 className="text-3xl font-bold mb-4">Posts</h1>
+            <h1 className="text-7xl font-bold mb-6 text-center">Posts</h1>
 
-            <div className="mb-4">
-                <input
-                    type="number"
-                    placeholder="Filtrar por userId"
-                    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}
-                    className="border rounded p-2 w-full md:w-1/3"
-                />
+            <div className="mb-8 p-4 flex justify-center">
+                <form className="w-full max-w-xl">
+                    <label
+                        htmlFor="user-search"
+                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                    >
+                        Filtrar por userId
+                    </label>
+                    <div className="relative">
+                        {/* Icono lupa */}
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg
+                                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                />
+                            </svg>
+                        </div>
+
+                        {/* Input controlado */}
+                        <input
+                            type="number"
+                            id="user-search"
+                            placeholder="Filtrar por userId..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 
+                   rounded-lg focus:ring-blue-500 focus:border-blue-500
+                    dark:border-gray-600 dark:placeholder-gray-400 
+                   dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                    </div>
+                </form>
             </div>
 
             {showLoading && (
-                <p className="text-yellow-600">
+                <p className="text-yellow-600 text-center mb-4">
                     Las solicitudes están tardando más de lo esperado...
                 </p>
             )}
 
-            {error && <p className="text-red-500">{error.message}</p>}
+            {error && <p className="text-red-500 text-center mb-4">{error.message}</p>}
 
-            <div className="grid gap-4 ">
-                <div className="grid gap-4 md:grid-cols-3 ">
-                    {posts?.map(post => (
-                        <Card key={post.id} post={post} />
-                    ))}
-                </div>
+            <div className="max-w-full max-h-32 grid gap-6 md:grid-cols-3">
+                {posts?.map(post => (
+                    <Card key={post.id} post={post} />
+                ))}
             </div>
         </main>
+
     );
 
 }
