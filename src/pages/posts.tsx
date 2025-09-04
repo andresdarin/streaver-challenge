f1b58f6553f5e77dfcce1b8536ca4e7e6a512e86
Andrés Darín <andres.darin05@gmail.com>
2025-09-04 18:39:45 -0300

    feat: add metadata for SEO

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -6,6 +6,13 @@ import { useState } from "react";
 import useDebounce from "@/hooks/useDebounce";
 import useDelayedLoading from "@/hooks/useDelayedLoading";
 import { POSTS_URL } from "@/utils/constants";
+import { Metadata } from "next";
+import Head from "next/head";
+
+export const metadata: Metadata = {
+    title: "Posts | X-plorer",
+    description: "Check out the most interesting posts and daily discoveries.",
+};
 
 export default function Posts() {
     const [searchTerm, setSearchTerm] = useState<string>("");
@@ -20,78 +27,87 @@ export default function Posts() {
     const showLoading = useDelayedLoading(isLoading, 500);
 
     return (
-        <main className="max-w-5xl mx-auto p-4  m-10 p-10 ">
-            <div className="min-w-full rounded-2xl p-4 shadow mb-4 group w-full bg-gradient-to-r from-green-100 via-blue-50 to-orange-100">
-                <h1 className="text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-600 to-blue-500
+        <>
+            <Head>
+                <title>Posts | X-plorer</title>
+                <meta
+                    name="description"
+                    content="Check out the most interesting posts and daily discoveries."
+                />
+            </Head>
+            <main className="max-w-5xl mx-auto p-4  m-10 p-10 ">
+                <div className="min-w-full rounded-2xl p-4 shadow mb-4 group w-full bg-gradient-to-r from-green-100 via-blue-50 to-orange-100">
+                    <h1 className="text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-600 to-blue-500
                            transition-all duration-500 cursor-default hover:from-pink-400 hover:via-purple-500 hover:to-blue-400
                            hover:scale-105">Posts X-plorer</h1>
-                <p className="text-center text-gray-700 dark:text-gray-400 mt-1">
-                    Check out the most interesting posts and daily discoveries.
-                </p>
-
-
-                <form className="p-4 flex justify-center">
-                    <label htmlFor="user-search" className="sr-only">Filtrar por userId</label>
-                    <input
-                        type="number"
-                        id="user-search"
-                        placeholder="Filtrar por userId..."
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        className="w-full p-4 text-sm text-gray-500 placeholder-gray-400 bg-gray-250/50 border rounded-2xl border-purple-400 focus:text-gray-900 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 hover:brightness-110"
-                    />
-                </form>
-
-                {/* Mensaje cuando no hay posts */}
-                {!isLoading && posts && posts.length === 0 && (
-                    <>
-                        {debouncedUserId ? (
-                            <p className="w-full pb-3 text-center text-red-500 dark:text-red-400 mt-2 font-medium">
-                                Looks like this user hasn’t posted anything.
-                            </p>
-                        ) : (
-                            <p className="w-full pb-3 text-center text-red-500 dark:text-red-400 mt-2 font-medium">
-                                No posts to show yet.
-                            </p>
-                        )}
-                    </>
-                )}
-
-                {showLoading && (
-                    <div>
-                        <p className="w-full pb-3 text-center text-gray-800 mt-2 font-medium">
-                            Almost there… your feed will be ready soon!
-                        </p>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-                            {Array.from({ length: 6 }).map((_, i) => (
-                                <div key={i} className="h-40 bg-gray-200 animate-pulse rounded-xl"></div>
-                            ))}
-                        </div>
-                    </div>
-                )}
-
-
-                {posts && posts.length > 0 && (
-                    <p className="text-center text-gray-900 dark:text-gray-300 mt-2 font-medium">
-                        {posts.length} posts
+                    <p className="text-center text-gray-700 dark:text-gray-400 mt-1">
+                        Check out the most interesting posts and daily discoveries.
                     </p>
-                )}
-            </div>
 
-            {error && <p className="text-red-500 text-center mb-4">{error.message}</p>}
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {posts?.map((post, index) => (
-                    <div
-                        key={post.id}
-                        className="break-inside-avoid"
-                    >
-                        <Card post={post} />
-                    </div>
-                ))}
-            </div>
-        </main>
+                    <form className="p-4 flex justify-center">
+                        <label htmlFor="user-search" className="sr-only">Filtrar por userId</label>
+                        <input
+                            type="number"
+                            id="user-search"
+                            placeholder="Filtrar por userId..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="w-full p-4 text-sm text-gray-500 placeholder-gray-400 bg-gray-250/50 border rounded-2xl border-purple-400 focus:text-gray-900 focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition-all duration-300 hover:brightness-110"
+                        />
+                    </form>
 
+                    {/* Mensaje cuando no hay posts */}
+                    {!isLoading && posts && posts.length === 0 && (
+                        <>
+                            {debouncedUserId ? (
+                                <p className="w-full pb-3 text-center text-red-500 dark:text-red-400 mt-2 font-medium">
+                                    Looks like this user hasn’t posted anything.
+                                </p>
+                            ) : (
+                                <p className="w-full pb-3 text-center text-red-500 dark:text-red-400 mt-2 font-medium">
+                                    No posts to show yet.
+                                </p>
+                            )}
+                        </>
+                    )}
+
+                    {showLoading && (
+                        <div>
+                            <p className="w-full pb-3 text-center text-gray-800 mt-2 font-medium">
+                                Almost there… your feed will be ready soon!
+                            </p>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                                {Array.from({ length: 6 }).map((_, i) => (
+                                    <div key={i} className="h-40 bg-gray-200 animate-pulse rounded-xl"></div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+
+
+                    {posts && posts.length > 0 && (
+                        <p className="text-center text-gray-900 dark:text-gray-300 mt-2 font-medium">
+                            {posts.length} posts
+                        </p>
+                    )}
+                </div>
+
+                {error && <p className="text-red-500 text-center mb-4">{error.message}</p>}
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {posts?.map((post, index) => (
+                        <div
+                            key={post.id}
+                            className="break-inside-avoid"
+                        >
+                            <Card post={post} />
+                        </div>
+                    ))}
+                </div>
+            </main>
+
+            );
+        </>
     );
-
 }
